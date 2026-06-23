8f341cd5798590098d306aabc84bf40c45380e2c
Ashwin-Kondoth <ashwinkondoth@gmail.com>
2026-06-23 12:55:11 +0530

    updated AXI transaction class

--- AXI_master_agent/axi_xtn.sv
+++ AXI_master_agent/axi_xtn.sv
@@ -27,4 +27,35 @@ class axi_xtn extends uvm_sequence_item;
 		super.new(name);
 	endfunction : new
 
+	rand bit [7:0]  awid;
+	rand bit [31:0] awaddr;
+	rand bit [7:0]  awlen;
+	rand bit [2:0]  awsize;
+	rand bit [1:0]  awburst;
+	rand bit        awvalid;
+		 bit        awready;
+	rand bit [7:0]  wid;
+	rand bit [63:0] wdata;
+		 bit [7:0]  wstrb[];
+	rand bit        wlast;
+	rand bit        wvalid;
+		 bit        wready;
+	rand bit [7:0]  arid;
+	rand bit [31:0] araddr;
+	rand bit [7:0]  arlen;
+	rand bit [2:0]  arsize;
+	rand bit [1:0]  arburst;
+	rand bit        arvalid;
+		 bit        arready;
+	rand bit [7:0]  bid;
+	rand bit [1:0]  bresp;
+	rand bit        bvalid;
+		 bit        bready;
+	rand bit [7:0]  rid;
+	rand bit [63:0] rdata;
+	rand bit [1:0]  rresp;
+	rand bit        rlast;
+	rand bit        rvalid;
+		 bit        rready;
+
 endclass : axi_xtn
